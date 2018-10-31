f8b2e4b47efeeb9da88b6786b4c7ef9a53daa5b1
Vudugun <vudugun@mail.com>
2018-10-31 20:56:34 +0100

    Change space geometry to sphere

--- code/space_aspect.js
+++ code/space_aspect.js
@@ -14,16 +14,16 @@ export class SpaceAspect extends Aspect {
   _initAnimations() {
     this._mesh.animations = [];
     this._mesh.animations.push(this._createSpaceAnimation());
-    this._screen.scene.beginAnimation(this._mesh, 0, 800, true);
+    this._screen.scene.beginAnimation(this._mesh, 0, 1200, true);
   }
   
   _createSpaceAnimation() {
-    const y = -(Math.PI / 2) + Math.random() * Math.PI;
+    const y = -Math.PI + Math.random() * Math.PI * 2;
     const from = new BABYLON.Vector3(0, y, 0);
     const to = new BABYLON.Vector3(Math.PI * 2, y, 0);
     const keys = [];
     keys.push({ frame: 0, value: from });
-    keys.push({ frame: 800, value: to });
+    keys.push({ frame: 1200, value: to });
     const anim = new BABYLON.Animation("rotate", "rotation", 1,
       BABYLON.Animation.ANIMATIONTYPE_VECTOR3,
       BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
